816127095e77be0748c43701615f3388d89551eb
SerhiiShymchuk <shymaser@gmail.com>
2021-08-01 19:42:25 +0300

    videoplayer

--- js/main.js
+++ js/main.js
@@ -37,8 +37,8 @@ window.addEventListener('DOMContentLoaded', () => {
     })
     feedSlider.init()
 
-    const videoplayer = new VideoPlayer('.showup .play', '.overlay')
-    videoplayer.init()
+    new VideoPlayer('.showup .play', '.overlay').init()
+    new VideoPlayer('.module__video-item .play', '.overlay').init()
 
     new Difference('.officerold', '.officernew', '.officer__card-item').init()
 
